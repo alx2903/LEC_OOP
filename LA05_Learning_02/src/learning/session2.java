d680280239191414b2fe23f3bc7eaeb25e75afdf
alx2903 <116934294+alx2903@users.noreply.github.com>
2022-10-29 11:09:17 +0700

    Add files via upload

---
+++ LA05_Learning_02/src/learning/session2.java
@@ -0,0 +1,88 @@
+package learning;
+
+import java.util.Scanner;
+
+public class session2 {
+
+	public session2() {
+		// TODO Auto-generated constructor stub
+//		int a = 10;		
+//		Integer b = 10;	
+//		b.toString();
+		
+//		Primitive		Wrapper
+//		char			Character
+//		short			Short
+//		int				Integer
+//		long			Long
+//		float			Float
+//		double			Double
+		
+//		int a = 10;
+//		Integer i = Integer.valueOf(a); //boxing
+//		int c = i.intValue(); //unboxing dia jadi primitive data lagi
+//		
+//		System.out.println("MAX VALUE: " + i.MAX_VALUE);
+		
+//		String word1 = "alx";
+//		String word2 = "alx";
+		
+//		System.out.println("Hasil Perbandingan, " + (word1 == word2));
+//		System.out.println("Hasil Perbandingan Kedua, " + word1.equals(word2));
+//		System.out.println("Hasil Perbandingan Ketiga, " + word1.compareTo(word2));
+		
+//		Integer angka1 = 10;
+//		Integer angka2 = 10;
+		
+//		System.out.println("Hasil Perbandingan, " + (angka1 == angka2));
+//		System.out.println("Hasil Perbandingan Kedua, " + angka1.equals(angka2));
+//		System.out.println("Hasil Perbandingan Ketiga, " + angka1.compareTo(angka2));
+		
+//		System.out.println("Length dari angka1 " + angka1.toString().length());
+		
+		double a = 2;
+		int b = 2;
+//		System.out.println(Math.PI);
+//		System.out.println(Math.sqrt(a));
+//		System.out.println(Math.sin(a+b));
+//		
+//		double deg = 57.0;
+//		System.out.println(Math.toRadians(deg));
+		
+//		System.out.println(Math.ceil(a));
+//		System.out.println(Math.floor(a));
+//		System.out.println(Math.round(a));
+		
+//		int angka1 = (int) (Math.random() * 10);
+//		if(angka1 == 1) {
+//			System.out.println("*");
+//		}else {
+//			System.out.println("<>");
+//		}
+//		System.out.println("Angka Random yang Keluar adalah : " + angka1);
+		
+//		System.out.println(a + b);
+//		System.out.println(a - b);
+//		System.out.println(a / b);
+//		System.out.println(a * b);
+//		System.out.println(a % b);
+		
+//		System.out.println((a + b) * 3);
+//		System.out.println(a + b * 3);
+		
+		int i = 1;
+		int d = i+=8;
+		System.out.println(d);
+		i++;
+		i--;
+		
+//		a.toString();
+//		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new session2();
+	}
+
+}
